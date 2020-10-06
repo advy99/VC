60974b2544df21c30fe1af04fedf13d9b220cd93
advy99 <advy99@gmail.com>
2020-10-06 18:52:26 +0200

    acabado codigo de la práctica 0

--- p0/p0.py
+++ p0/p0.py
@@ -43,12 +43,12 @@ def lee_imagen_fichero(fichero, flag_color):
 def mostrar_imagen(imagen, titulo=""):
     cv.namedWindow(titulo, cv.WINDOW_NORMAL)
     cv.imshow(titulo, imagen)
-    cv.waitKey(0)
+    cv.waitKey()
     cv.destroyAllWindows()
 
-  # Si OpenCV da error
-  # plt.imshow(imagen)
-  # plt.show()
+	 # Si OpenCV da error
+	 # plt.imshow(imagen)
+	 # plt.show()
 
 def leeimagen(fichero, color_flag):
     imagen = lee_imagen_fichero(fichero, color_flag)
@@ -87,7 +87,7 @@ el rango de cada banda al intervalo [0,1] sin pérdida de información.
 print("Ejercicio 2:")
 
 
-def pintaI(imagen):
+def normaliza_imagen(imagen):
 
     # trabajamos en una copia de la imagen
     img_normalizada = np.copy(imagen)
@@ -119,14 +119,16 @@ def pintaI(imagen):
 
     return img_normalizada
 
-
+def pintaI(imagen):
+    normalizada = normaliza_imagen(imagen)
+    mostrar_imagen(normalizada)
 
 #imagen = lee_imagen_fichero("imagenes/orapple.jpg", 1)
 
 print("Probando con matriz aleatoria como imagen monobanda")
 imagen_monobanda = np.random.randn(500,500,1) * 5
 
-mostrar_imagen(pintaI(imagen_monobanda))
+pintaI(imagen_monobanda)
 
 input("\n-------Pulsa una tecla para continuar-------\n")
 
@@ -134,14 +136,15 @@ input("\n-------Pulsa una tecla para continuar-------\n")
 print("Probando con matriz aleatoria como imagen tribanda")
 imagen_tribanda = np.random.randn(500,500,3) * 5
 
-mostrar_imagen(pintaI(imagen_tribanda))
+pintaI(imagen_tribanda)
 
 input("\n-------Pulsa una tecla para continuar-------\n")
 
 print('Probando con una de las imagenes dadas')
 imagen = lee_imagen_fichero("imagenes/orapple.jpg",1)
 
-mostrar_imagen(pintaI(imagen))
+pintaI(imagen)
+
 input("\n-------Pulsa una tecla para continuar-------\n")
 
 
@@ -160,7 +163,7 @@ color, blanco-negro)?
 print("Ejercicio 3")
 
 
-def pintaIM(vim):
+def une_imagenes(vim):
 
     num_imagenes = len(vim)
 
@@ -171,30 +174,34 @@ def pintaIM(vim):
 
     altura_maxima = np.max( alturas )
 
-        # cogemos la primera imagen normalizada
-    imagen_final = pintaI(vim[0])
+    # cogemos la primera imagen normalizada
+    imagen_final = normaliza_imagen(vim[0])
 
     if imagen_final.shape[0] < altura_maxima:
         filas_restantes = altura_maxima - vim[0].shape[0]
         franja_negra = np.ones( (filas_restantes, vim[0].shape[1]))
-        franja_negra = pintaI(franja_negra)
+        franja_negra = normaliza_imagen(franja_negra)
         imagen_final = np.vstack((imagen_final, franja_negra ))
 
 
     for i in range(1, num_imagenes):
         # para las siguientes imagenes, las normalizamos
-        img = pintaI(vim[i])
+        img = normaliza_imagen(vim[i])
 
         # si les faltan filas, añadimos las restantes como un borde negro
         if img.shape[0] < altura_maxima:
             filas_restantes = altura_maxima - img.shape[0]
             franja_negra = np.ones( (filas_restantes, img.shape[1]))
-            franja_negra = pintaI(franja_negra)
+            franja_negra = normaliza_imagen(franja_negra)
             img = np.vstack((img, franja_negra ))
 
         imagen_final = np.hstack((imagen_final, img))
 
     return imagen_final
+
+def pintaMI(imagenes, titulo=None):
+    fusion = une_imagenes(imagenes)
+    mostrar_imagen(fusion, titulo)
 
 print("Uniendo imagenes orapple.jpg y messi.jpg")
 
@@ -203,9 +210,8 @@ imagen2 = lee_imagen_fichero("imagenes/messi.jpg", 1)
 
 imagenes = [imagen2, imagen1]
 
-imagen_unida = pintaIM(imagenes)
 
-mostrar_imagen(imagen_unida)
+pintaMI(imagenes)
 
 
 input("\n-------Pulsa una tecla para continuar-------\n")
@@ -238,7 +244,7 @@ def modificar_color(imagen, coordenadas_a_modificar, nuevo_color):
     return resultado
 
 imagen = lee_imagen_fichero("imagenes/orapple.jpg", 1)
-imagen = pintaI(imagen)
+imagen = normaliza_imagen(imagen)
 
 color = np.array([0.74509803921, 0.74901960784, 0.96862745098], np.float64)
 
@@ -252,4 +258,35 @@ for i in range(0,50):
 resultado_ej4 = modificar_color(imagen, coordenadas, color)
 
 mostrar_imagen(resultado_ej4)
+
+
+input("\n-------Pulsa una tecla para continuar-------\n")
+
+"""
+
+Ejercicio 5:
+
+Una función que sea capaz de representar varias imágenes con sus títulos en una misma ventana.
+
+"""
+
+print("Ejercicio 5:")
+
+def pintaMI_con_titulo(imagen, titulo):
+    pintaMI(imagenes, titulo)
+    return
+
+print("Uniendo imagenes orapple.jpg y messi.jpg (ahora con titulo)")
+
+imagen1 = lee_imagen_fichero("imagenes/orapple.jpg", 1)
+imagen2 = lee_imagen_fichero("imagenes/messi.jpg", 1)
+
+imagenes = [imagen2, imagen1]
+
+
+pintaMI(imagenes, "Ejercicio 5: El 3 pero con titulo de ventana")
+
+
+input("\n-------Pulsa una tecla para continuar-------\n")
+
 
