1f4d961d422598db5b27a6a71b1141bc189d477a
advy99 <advy99@gmail.com>
2020-10-17 22:34:14 +0200

    apartado A del ejercicio 1

--- p1/p1.py
+++ p1/p1.py
@@ -12,6 +12,7 @@ Antonio David Villegas Yeguas
 import cv2 as cv
 import matplotlib as plt
 import numpy as np
+import math
 
 """
 
@@ -68,24 +69,62 @@ def segunda_derivada_f_gaussiana(x, sigma):
 
     return (- ( (-x**2 + sigma**2) / ( math.exp((x**2) / (2*sigma**2)) * sigma**4 ) ) )
 
-def kernel_gaussiano_1d(sigma, func=funcion_gaussiana):
+def kernel_gaussiano_1d(sigma=None, func=funcion_gaussiana, tam_mascara=None):
 
-    # usaremos un intervalo de [-3 * sigma, 3 * sigma] para obtener el kernel
-    comienzo_intervalo = int(- (3 * sigma))
-    fin_intervalo = -comienzo_intervalo
+    parametros_correctos = tam_mascara != None or sigma != None
+
+    if tam_mascara != None:
+        sigma = tam_mascara - 1 / 6
+    elif sigma != None:
+        # usaremos un intervalo de  para obtener el kernel
+        tam_mascara = 2 * 3 * sigma + 1
+    else:
+        print("ERROR: Debes aportar un sigma o un tamaño de máscara")
+
 
     kernel = []
+    kernel_normalizado = []
 
-    # usaremos fin_intervalo+1 para que fin_intervalo este incluido
-    for x in range(comienzo_intervalo, fin_intervalo + 1):
-        kernel.append( func(x, sigma) )
+    if parametros_correctos:
 
-    # normalizamos el kernel dividiendo cada valor por la suma de todos
-    # haciendo que en total sumen 1
-    kernel_normalizado = kernel / np.sum(kernel)
+        mitad_intervalo = np.floor(tam_mascara/2)
+        mitad_intervalo = int(mitad_intervalo)
+
+        # usamos la mitad ya que si tam_mascara vale x, en realidad vamos desde
+        # -x/2 .. x/2, teniendo el total un tamaño de x
+        for x in range(-mitad_intervalo, mitad_intervalo + 1):
+            kernel.append( func(x, sigma) )
+
+        # normalizamos el kernel dividiendo cada valor por la suma de todos
+        # haciendo que en total sumen 1
+        kernel_normalizado = kernel
+
+        # si la suma de los valores del kernel no es 0 lo normalizamos
+        # para que la suma sea 1
+        # en lugar de comparar con 0, comparamos si el valor absoluto es mayor
+        # que cierto epsilon ya que comprar con 0 puede tener problemas de coma
+        # flotante y por lo tanto comportamientos inesperados
+        if abs(np.sum(kernel)) > 0.005:
+            kernel_normalizado = kernel_normalizado / np.sum(kernel)
 
     return kernel_normalizado
 
+
+mascara_gaussiana_sigma_1 = kernel_gaussiano_1d(1)
+mascara_primera_deriv_t_5 = kernel_gaussiano_1d(None, derivada_f_gaussiana, 5)
+mascara_segunda_deriv_t_7 = kernel_gaussiano_1d(None, derivada_f_gaussiana, 7)
+
+print("Máscara obtenida de la función gaussiana con sigma = 1: ")
+print(mascara_gaussiana_sigma_1)
+
+print("\nMáscara obtenida de la función derivada de la gaussiana con tam. mascara = 5: ")
+print(mascara_primera_deriv_t_5)
+
+print("\nMáscara obtenida de la función segunda derivada de la gaussiana con tam. mascara = 7: ")
+print(mascara_segunda_deriv_t_7)
+
+
+input("\n---------- Pulsa una tecla para continuar ----------\n")
 
 bicycle = leeimagen("imagenes/bicycle.bmp", 0)
 bird = leeimagen("imagenes/bird.bmp", 0)
