9f244a24e6f27173307de94b4d8da0e32eef4922
advy99 <advy99@gmail.com>
2020-12-25 13:53:02 +0100

    dibujamos los puntos sobre la imagen a color

--- p3/p3.py
+++ p3/p3.py
@@ -414,11 +414,11 @@ Apartado 1
 """
 
 yosemite_1_bn = leeimagen("imagenes/Yosemite1.jpg", 0)
-yosemite_2_bn = leeimagen("imagenes/Yosemite2.jpg", 0)
+yosemite_1_color = leeimagen("imagenes/Yosemite1.jpg", 1)
 
 puntos, puntos_corregidos = puntos_harris(yosemite_1_bn, tam_bloque = 5, tam_ventana = 3, num_escalas = 3, sigma_p_gauss = 4.5, umbral_harris = 10.0, ksize = 3)
 
 
 
-dibujar_puntos_harris(yosemite_1_bn, puntos)
+dibujar_puntos_harris(yosemite_1_color, puntos)
 
